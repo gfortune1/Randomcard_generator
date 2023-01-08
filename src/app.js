6eca574dda4da962ecee300aa403785d63848c56
gfortune1 <fgeee1@gmail.com>
2023-01-08 23:48:05 +0000

    final

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let numberlist = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let randomnumber = numberlist[Math.floor(Math.random() * numberlist.length)];
+  let number = document.querySelector(".number");
+  number.innerHTML = randomnumber;
+  let suitlist = ["♦", "♥", "♠", "♣"];
+  let randomsuit = suitlist[Math.floor(Math.random() * suitlist.length)];
+  let suit = document.querySelectorAll(".suit");
+  suit.forEach(element => {
+    element.innerHTML = randomsuit;
+  });
 };
